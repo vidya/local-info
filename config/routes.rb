f868db66d39aa58c9558fc48c289e70494405d95
vidya <vsagarj@gmail.com>
2012-09-25 19:13:49 -0700

    Change root to home#index.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ LocalInfo::Application.routes.draw do
     root :to => 'home#index'
   end
 
-  root :to => "users#index"
+  #root :to => "users#index"
+  root :to => "home#index"
 
   devise_for :users
   resources :users, :only => [:show, :index]
