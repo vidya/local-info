3d4c1221b0aea383fca9a39971e2bcb8aff00a45
vidya <vsagarj@gmail.com>
2012-09-28 18:05:20 -0700

    Minor formatting change.

--- db/migrate/20120910005837_create_zip_codes.rb
+++ db/migrate/20120910005837_create_zip_codes.rb
@@ -4,8 +4,8 @@ class CreateZipCodes < ActiveRecord::Migration
       t.string      :zip,         :null => false,   :limit => 5
       t.string      :state_code,  :null => false,   :limit => 2
 
-      t.float      :latitude,    :null => false,   :limit => 10
-      t.float      :longitude,   :null => false,   :limit => 10
+      t.float       :latitude,    :null => false,   :limit => 10
+      t.float       :longitude,   :null => false,   :limit => 10
 
       t.string      :city,                          :limit => 30
       t.string      :state,       :null => false,   :limit => 20
