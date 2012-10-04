436c7362fe2d7475957d2521a366ccd4d0ff9979
vidya <vsagarj@gmail.com>
2012-10-04 12:11:18 -0700

    Cleanup test code.
    state_test.rb
     - remove tests in Test:Unit format
     - replace them with minitest specs
    
    zip_code_test.rb
     - remove outdated comments
     - add a TODO to add more tests

--- test/unit/state_test.rb
+++ test/unit/state_test.rb
@@ -1,43 +1,14 @@
-#require 'test_helper'
-#
-#class StateTest < ActiveSupport::TestCase
-#  # test "the truth" do
-#  #   assert true
-#  # end
-#end
+describe State do
+  it "should return CA as code for California" do
+    st = State.find_by_name 'California'
 
-require 'test_helper'
-#require "minitest/autorun"
-#require "minitest-rails"
-
-class StateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-#end
-
-
-#class StateTest < MiniTest::Unit::TestCase
-#  def test_sanity
-#    puts "FROM test_sanity"
-#    assert true
-#    assert MiniTest::Rails::VERSION
-    #binding.pry
-    #true
-  #end
-
-  def test_includes_hawaii
-    #puts "--- test_includes_hawaii"
-    states = State.all.map { |st| st.name }
-    states.must_include 'Hawaii'
+    st.code.must_equal 'CA'
   end
 
-  def test_includes_sample_states
-    #puts "--- test_includes_sample_states"
+  it "should include sample states" do
     sample_states = ['Hawaii', 'New Jersey', 'Kentucky', 'Wisconsin'].map { |st| st.downcase }
     all_states = State.all.map { |st| st.name.downcase }
 
-    #binding.pry
     (sample_states - all_states).must_be_empty
   end
 end
