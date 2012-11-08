e4ca7ea333d1d397ac9c3d1f03c52b701f3c95f3
vidya <vsagarj@gmail.com>
2012-11-07 19:41:34 -0800

    Check validity of zip code in show area action
    home_controller.rb
     - in show area action, check if the entered zip code is valid

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,10 +10,45 @@ class HomeController < ApplicationController
 
   def show_area
     @params                   = params
-    @params[:theatre_profiles]   = get_theatres params
+    @user                     = current_user
+
+    @query = {
+      query_type:      'zip_code',
+
+      zip_code:        params[:zip_code],
+    }
+
+    if valid_query? @query
+      flash.clear
+
+      @params[:theatre_profiles]   = get_theatres params
+
+    else
+      flash[:error] = @query_error
+      redirect_to  action: :index
+    end
   end
 
   def add_neighbor
     render :text => 'FROM add_neighbor'
+  end
+
+  private
+  def valid_query?(query)
+    @query_error = nil
+
+    errors = []
+    case query[:query_type]
+      when 'zip_code'
+        zip_code   = query[:zip_code]
+        errors    << "invalid zip code (= #{zip_code})" unless ZipCode.valid_zip_code?(zip_code)
+
+      else
+        raise "--- ERROR: unexpected query_type = #{query[:query_type]}"
+    end
+
+    @query_error = 'ERROR(S): [ ' + errors.join(', ') + ' ]' unless errors.empty?
+
+    errors.empty?
   end
 end
